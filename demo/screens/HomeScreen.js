d5941d8d9356ed65aed05b3e9137d271f6a66acc
Douglas Lee <lee010073@gmail.com>
2022-07-10 19:05:53 +0800

    updated modal

--- demo/screens/HomeScreen.js
+++ demo/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import React from "react";
 const HomeScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>HomeScreen</Text>
     </View>
   );
 };
--- demo/screens/LoginScreen.js
+++ demo/screens/LoginScreen.js
@@ -5,14 +5,27 @@ import {
   TouchableOpacity,
   TextInput,
   Image,
+  Button,
 } from "react-native";
-import React, { Component, useState, useContext } from "react";
+import React, { Component, useState, useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/authContext";
+import Modal from "react-native-modal";
 
 const LoginScreen = () => {
   const context = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
+  const [isModalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  useEffect(() => {
+    if (!!context.loginSuccess) {
+      setModalVisible(false);
+    }
+  }, [context.loginSuccess]);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Image
@@ -21,24 +34,47 @@ const LoginScreen = () => {
           uri: "https://reactnative.dev/img/tiny_logo.png",
         }}
       />
-      <Text>Logsin</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setEmail}
-        value={email}
-        placeholder="email"
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setPw}
-        value={pw}
-        placeholder="password"
-        keyboardType="default"
-      />
-      <TouchableOpacity onPress={() => context.checking(email, pw)}>
-        <Text>Press Here</Text>
-      </TouchableOpacity>
+      <Button title="Show modal" onPress={toggleModal} />
+
+      <Modal
+        isVisible={isModalVisible}
+        onBackdropPress={() => setModalVisible(false)}
+      >
+        <View style={{ height: 200, backgroundColor: "white" }}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="email"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setPw}
+            value={pw}
+            placeholder="password"
+            keyboardType="default"
+          />
+          <TouchableOpacity
+            style={{ alignItems: "center", justifyContent: "center" }}
+            onPress={() => {
+              context.checking(email, pw);
+            }}
+          >
+            <Text>Confirm Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              marginTop: 10,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            onPress={toggleModal}
+          >
+            <Text>Hide modal</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
